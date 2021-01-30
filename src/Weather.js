84afb834a9c3fc9342feb0a565d7e79089d2fd54
BlancaPerezSoler <75004056+BlancaPerezSoler@users.noreply.github.com>
2021-01-30 11:46:44 +0100

    Added search engine and basic data on Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,40 @@
 import React from "react";
-
+import "./Weather.css";
 export default function Weather(){
     return(
         <div className= "Weather">
+       <form>
+           <div className="row">
+               <div className="col-9">
+        <input type="search"  placeholder="Typer city..." autoFocus="on" className="form-control"/>
+               </div>
+            <div className="col-3">
+        <input type="submit" className="btn btn-primary"/>
+            </div>
+           </div>
+        </form>
+
         <h1>Amsterdam</h1>
         <h5>Saturday 10.30</h5>
         <h5>Cloudy</h5>
-        </div>
+ 
+ <div className="row">
+     <div className="col-3">
+         <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="cloudy"/>
+     </div>
+     <div className="col-3">
+         6ºC
+     </div>
+<div className="col-6">
+    <ul>
+        <li>Humidity: 20%</li>
+        <li>Wind: 5 km/h</li>
+    </ul>
+</div>
+ </div>
+
+
+    </div>
+
     );
 }
