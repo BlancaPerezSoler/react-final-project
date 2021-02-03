1ed1e0c674bae9eb26416c50f3b6dd4d9a79ccb1
BlancaPerezSoler <75004056+BlancaPerezSoler@users.noreply.github.com>
2021-02-03 13:09:35 +0100

    new grid temperature

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -14,16 +14,16 @@ export default function CurrentWeather(props){
         <h5 className="text-capitalize">{props.data.description}</h5>
  
  <div className="row">
-     <div className="col-3" xs="auto" md="auto">
+     <div className="col-3" xs={6} md={4}>
          
          <img src={props.data.icon} alt="icon weather"/>
 
  </div>    
- <div className="col-3" xs="auto" md="auto">
+ <div className="col-3" xs={6} md={4}>
          <WeatherTemperature celsius={props.data.temperature}/>
 </div>
      
-<div className="col-6" xs="auto"md="auto">
+<div className="col-6" xs={6} md={4}>
     <ul>
         <li>Humidity: {props.data.humidity}%</li>
         <li>Wind: {Math.round(props.data.wind)} km/h</li>
