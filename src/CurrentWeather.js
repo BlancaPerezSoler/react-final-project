5efd25e76b82544988ef0320f402f464ffe1588a
BlancaPerezSoler <75004056+BlancaPerezSoler@users.noreply.github.com>
2021-02-03 11:43:12 +0100

    new layout

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -14,16 +14,16 @@ export default function CurrentWeather(props){
         <h5 className="text-capitalize">{props.data.description}</h5>
  
  <div className="row">
-     <div className="col-2">
+     <div className="col-3">
          
          <img src={props.data.icon} alt="icon weather"/>
 </div>
-<div className="col-5">
+<div className="col-3">
          <WeatherTemperature celsius={props.data.temperature}/>
          
      </div>
      
-<div className="col-5">
+<div className="col-6">
     <ul>
         <li>Humidity: {props.data.humidity}%</li>
         <li>Wind: {Math.round(props.data.wind)} km/h</li>
