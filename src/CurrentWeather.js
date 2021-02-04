3261f14b75f837925751dad10a563eb0f18f028a
BlancaPerezSoler <75004056+BlancaPerezSoler@users.noreply.github.com>
2021-02-04 07:13:43 +0100

    changed grid for mobile view

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -14,16 +14,16 @@ export default function CurrentWeather(props){
         <h5 className="text-capitalize">{props.data.description}</h5>
  
  <div className="row">
-     <div className="col-3" xs={6} md={4}>
+     <div className="col-6 col-md-4 col-lg-3">
          
          <img src={props.data.icon} alt="icon weather"/>
 
  </div>    
- <div className="col-3" xs={6} md={4}>
+ <div className="col-6 col-md-4 col-lg-3">
          <WeatherTemperature celsius={props.data.temperature}/>
 </div>
      
-<div className="col-6" xs={6} md={4}>
+<div className="col-6 col-md-4 col-lg-3">
     <ul>
         <li>Humidity: {props.data.humidity}%</li>
         <li>Wind: {Math.round(props.data.wind)} km/h</li>
