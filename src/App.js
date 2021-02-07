72f3b6fa7c15e14762bb726d7cbc507e755c8e3a
BlancaPerezSoler <75004056+BlancaPerezSoler@users.noreply.github.com>
2021-02-07 12:24:59 +0100

    fixed typo

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
    
   </div> 
   <footer>
-   This page was reated by <a style={{marginRight:"6px", marginLeft:"6px"}} href="https://www.linkedin.com/in/blancaperezsoler/" target="_blank" rel="noreferrer">Blanca Perez Soler </a> {" "} with <a style={{marginLeft:"6px"}} href="https://github.com/BlancaPerezSoler/react-final-project" target="_blank"rel="noreferrer">open-source at GitHub</a>
+   This page was created by <a style={{marginRight:"6px", marginLeft:"6px"}} href="https://www.linkedin.com/in/blancaperezsoler/" target="_blank" rel="noreferrer">Blanca Perez Soler </a> {" "} with <a style={{marginLeft:"6px"}} href="https://github.com/BlancaPerezSoler/react-final-project" target="_blank"rel="noreferrer">open-source at GitHub</a>
     </footer>
   </div>
   );  
